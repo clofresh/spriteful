1aa30ba3d4f953df31c7a68123d5d1875c056ccf
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-09-06 19:39:31 -0400

    namespace the game functions

--- static/js/game.js
+++ static/js/game.js
@@ -1,41 +1,53 @@
-function find_path(start, end, path) {
-  // Eventually implement this as A*
+var game = {
+  find_path: function(start, end, path) {
+    // Eventually implement this as A*
 
-  console.log(_.template('Finding path from (<%= start.row %>, <%= start.col %>) to (<%= end.row %>,<%= end.col %>)', 
-    {
-      start: start,
-      end: end
-    }
-  ));
+    console.log(_.template('Finding path from (<%= start.row %>, <%= start.col %>) to (<%= end.row %>,<%= end.col %>)', 
+      {
+        start: start,
+        end: end
+      }
+    ));
   
-  var equals = function(a, b) { return (a.row == b.row && a.col == b.col) };
-  var new_path = path;
+    var equals = function(a, b) { return (a.row == b.row && a.col == b.col) };
+    var new_path = path;
   
-  if (equals(start, end)) {
-    new_path.push(end);
-    return path;
-  } else {
-    var step;
-    if (end.row > start.row) {
-      step = {row: 1, col: 0};
-    } else if (end.row < start.row) {
-      step = {row: -1, col: 0};
-    } else if (end.col > start.col) {
-      step = {row: 0, col: 1};
-    } else if (end.col < start.col) {
-      step = {row:0, col: -1};
-    }
+    if (equals(start, end)) {
+      new_path.push(end);
+      return path;
+    } else {
+      var step;
+      if (end.row > start.row) {
+        step = {row: 1, col: 0};
+      } else if (end.row < start.row) {
+        step = {row: -1, col: 0};
+      } else if (end.col > start.col) {
+        step = {row: 0, col: 1};
+      } else if (end.col < start.col) {
+        step = {row:0, col: -1};
+      }
     
-    next = {
-      row: start.row + step.row,
-      col: start.col + step.col
+      next = {
+        row: start.row + step.row,
+        col: start.col + step.col
+      };
+    
+      new_path.push(start);
+    
+      return game.find_path(next, end, new_path);
+    }
+  },
+
+  init_clock: function(beat_interval) {
+    var heartbeat = function() {
+      $('.actor').trigger('g:heartbeat');
+      window.setTimeout(heartbeat, beat_interval);
     };
-    
-    new_path.push(start);
-    
-    return find_path(next, end, new_path);
-  }
-}
+  
+    window.setTimeout(heartbeat, beat_interval);
+  }  
+};
+
 
 (function($) {
   $.fn.pathTo = function(end_selector) {
@@ -51,7 +63,7 @@ function find_path(start, end, path) {
       col: $dest.data('col')
     }
   
-    var path = find_path(start, end, []);
+    var path = game.find_path(start, end, []);
     
     return $(_(path).map(function(o) {
       var sel = _.template('.tile.row-<%= row %>.col-<%= col %>', o);
@@ -60,11 +72,4 @@ function find_path(start, end, path) {
   };
 })(jQuery);
 
-function init_clock(beat_interval) {
-  var heartbeat = function() {
-    $('.actor').trigger('g:heartbeat');
-    window.setTimeout(heartbeat, beat_interval);
-  };
-  
-  window.setTimeout(heartbeat, beat_interval);
-}
+
